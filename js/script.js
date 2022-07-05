120edaf636afd48e35e4d4d02d61ae42237dbf4f
MrGians <gianlumura97@gmail.com>
2022-07-05 13:18:32 +0200

    Boilerplate

---
+++ js/script.js
@@ -0,0 +1,27 @@
+/*
+  TRACCIA
+  Creare tramite un array di oggetti un layout "Our Team" sul DOM,
+  contenente delle card con i singoli membri ed i loro dati mostrati al suo interno.
+*/
+
+// # MILESTONE 0:
+// Creare l’array di oggetti con le informazioni fornite.
+// # MILESTONE 1:
+// Stampare su console le informazioni di nome, ruolo e la stringa della foto
+// # MILESTONE 2:
+// Stampare le stesse informazioni su DOM sottoforma di stringhe
+// # BONUS 1:
+// Trasformare la stringa foto in una immagine effettiva (le trovate in allegato)
+// # BONUS 2:
+// Organizzare i singoli membri in card/schede e ispirarci alle grafiche in allegato
+
+/* # DATI
+
+Wayne Barnett |	Founder & CEO        |	wayne-barnett-founder-ceo.jpg
+Angela Caroll |	Chief Editor         |	angela-caroll-chief-editor.jpg
+Walter Gordon |	Office Manager       |	walter-gordon-office-manager.jpg
+Angela Lopez  |	Social Media Manager |	angela-lopez-social-media-manager.jpg
+Scott Estrada |	Developer            |	scott-estrada-developer.jpg
+Barbara Ramos |	Graphic Designer     |	barbara-ramos-graphic-designer.jpg
+
+*/
